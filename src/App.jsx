d63c47628aea2e6c1ff09cb150b5d53718e4a2af
Coding rev <fernando.lobo.prez@gmail.com>
2025-03-11 11:40:59 -0300

    03-creating routes
    03-creating routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
+import { BrowserRouter, Route, Routes } from 'react-router'
+
+/// COMPONENTS
+import Home from './components/home';
+import Posts from './components/posts';
+import Profile from './components/profile';
 
 function App() {
-  
   return (
-    <>
-      App
-    </>
+    <BrowserRouter>
+      <header>
+        Header
+      </header>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="posts" element={<Posts/>}/>  
+        <Route path="profile" element={<Profile/>}/>  
+      </Routes>  
+    </BrowserRouter>
   )
 }
 
