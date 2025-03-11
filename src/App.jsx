0995290d32ac407422ccbfa2710a5f5f0cb6ca86
Coding rev <fernando.lobo.prez@gmail.com>
2025-03-11 12:08:34 -0300

    05-A better Header
    05-A better Header

--- src/App.jsx
+++ src/App.jsx
@@ -8,20 +8,30 @@ import Profile from './components/profile';
 function App() {
   return (
     <BrowserRouter>
-      <header>
-        <Link to="/">Home</Link><br/>
-        <Link to="posts">Posts</Link><br/>
-        <Link to={{
-          pathname:'profile',
-          hash:'#francis',
-          search:'?profile=true'
-        }}>Profile</Link><br/>
-      </header>
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="posts" element={<Posts/>}/>  
-        <Route path="profile" element={<Profile/>}/>  
-      </Routes>  
+      <div className='container'>
+        <header className='d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom'>
+          <Link to="/" className='d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none'>
+            <span className='fs-4'>My App</span>
+          </Link>
+
+          <ul className='nav nav-pills'>
+            <li className='nav-item'>
+              <Link to="/" className='nav-link'>Home</Link>
+            </li>
+            <li className='nav-item'>
+              <Link to="posts" className='nav-link'>Posts</Link>
+            </li>
+            <li className='nav-item'>
+              <Link to="profile" className='nav-link'>Profile</Link>
+            </li>
+          </ul>
+        </header>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="posts" element={<Posts/>}/>  
+          <Route path="profile" element={<Profile/>}/>  
+        </Routes>  
+      </div>
     </BrowserRouter>
   )
 }
